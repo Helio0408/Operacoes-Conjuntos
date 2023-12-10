c3f845469476ce1483c3c39bf786f76fc1d918e8
Hélio Santos <128417028+Helio0408@users.noreply.github.com>
2023-12-09 22:32:12 -0300

    Add files via upload

---
+++ src/set.c
@@ -0,0 +1,38 @@
+#include "set.h"
+
+struct set{
+
+};
+
+
+SET *set_criar(void){
+
+}
+
+bool set_pertence(SET *A, int elemento){
+
+}
+
+bool set_inserir (SET *s, int elemento){
+
+}
+
+bool set_remover(SET *s, int elemento){
+
+}
+
+void set_apagar(SET **s){
+
+}
+
+void set_imprimir(SET *s){
+
+}
+
+SET *set_uniao(SET *A, SET *B){
+
+}
+
+SET *set_interseccao(SET *A, SET *B){
+
+}
