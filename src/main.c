7673cb59a52371694b4dffe17c371715126ecd1d
Gabriel Martins <gabrielmartinsthe12381@gmail.com>
2023-12-11 14:10:20 -0300

    Updated main

--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "set.h"
 
 int main(){
 	srand(time(NULL));
 	SET *A, *B;
   	int n_a, n_b, x;
-  	int op, num;
+  	int num;
 	int quit = 0;
+	char op[20];
+	char conjunto_atual;
 
 	A = set_criar();
 	B = set_criar();
@@ -26,54 +29,62 @@ int main(){
   	}
 
 	while(!quit){
-  		scanf("%d", &op);
+  		scanf("%s", op);
 
-		switch(op){
-			case 0: {
-				quit = 1;
-				break;
+		if(!strcmp(op, "quit")){
+			quit = 1;
+		}
+		else if(!strcmp(op, "pertence")){
+			scanf(" %c %d", &conjunto_atual, &num);
+
+			if(conjunto_atual == 'a'){
+				if(set_pertence(A, num)) printf("%d pertence ao conjunto a\n", num);
+				else printf("%d nao pertence oa conjunto a\n", num);
 			}
-			case 1: {
-				scanf("%d", &num);
-				if(set_pertence(A,num)) 
-					printf("Pertence.\n");
-				else
-					printf("Nao Pertence.\n");
-				break;
-    		}
-    		case 2: {
-				SET *C = set_uniao(A,B);
-				printf("Uniao: ");
-				set_imprimir(C);
-				set_apagar(&C);
-				break;
-    		}
-    		case 3: {
-				SET *C = set_interseccao(A, B);
-				printf("Interseccao: ");
-				set_imprimir(C);
-				set_apagar(&C);
-				break;
-    		}
-    		case 4: {
-				int num;
-				scanf("%d", &num);
+			else{
+				if(set_pertence(B, num)) printf("%d pertence ao conjunto b\n", num);
+				else printf("%d nao pertence oa conjunto b\n", num);
+			}
+		}
+		else if(!strcmp(op, "uniao")){
+			SET *C = set_uniao(A,B);
+			printf("uniao: ");
+			set_imprimir(C);
+			set_apagar(&C);
+		}
+		else if(!strcmp(op, "interseccao")){
+			SET *C = set_interseccao(A, B);
+			printf("interseccao: ");
+			set_imprimir(C);
+			set_apagar(&C);
+		}
+		else if(!strcmp(op, "inserir")){
+			scanf(" %c %d", &conjunto_atual, &num);
+
+			if(conjunto_atual == 'a'){
+				set_inserir(A,num);
+			}
+			else{
+				set_inserir(B,num);
+			}
+		}
+		else if(!strcmp(op, "remover")){
+			scanf(" %c %d", &conjunto_atual, &num);
+
+			if(conjunto_atual == 'a'){
 				set_remover(A,num);
-				set_imprimir(A); 
-				break;
-    		}
-    		case 5: {
-				printf("A: ");
-				set_imprimir(A); 
-				printf("B: ");
-				set_imprimir(B); 
-				break;
-    		}
+			}
+			else{
+				set_remover(B,num);
+			}
+		}
+		else if(!strcmp(op, "imprimir")){
+			printf("a: ");
+			set_imprimir(A);
+			printf("b: ");
+			set_imprimir(B);
 		}
 	}
-
-	set_imprimir(A);
-	set_imprimir(B);
 
 	set_apagar(&A);
 	set_apagar(&B);
